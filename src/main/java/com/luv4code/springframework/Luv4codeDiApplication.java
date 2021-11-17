9cfeb826f4ddb713697ede5081c92b01b340da31
luv4boot <luv4boot@gmail.com>
2021-11-17 18:07:00 +0530

    Initial commit

---
+++ src/main/java/com/luv4code/springframework/Luv4codeDiApplication.java
@@ -0,0 +1,13 @@
+package com.luv4code.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Luv4codeDiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Luv4codeDiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luv4code/springframework/Luv4codeDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv4code.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Luv4codeDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
