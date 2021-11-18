f68c419b4eef2f5e9376de5d79c6a21b96ed091d
luv4boot <luv4boot@gmail.com>
2021-11-18 10:36:19 +0530

    using spring profiles

--- src/main/java/com/luv4code/springframework/Luv4codeDiApplication.java
+++ src/main/java/com/luv4code/springframework/Luv4codeDiApplication.java
@@ -1,9 +1,6 @@
 package com.luv4code.springframework;
 
-import com.luv4code.springframework.controllers.ConstructorInjectedController;
-import com.luv4code.springframework.controllers.MyController;
-import com.luv4code.springframework.controllers.PropertyInjectedController;
-import com.luv4code.springframework.controllers.SetterInjectedController;
+import com.luv4code.springframework.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +10,11 @@ public class Luv4codeDiApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Luv4codeDiApplication.class, args);
+
+        I18NController i18NController = (I18NController) context.getBean("i18NController");
+        System.out.println(i18NController.sayHello());
+
+
         MyController controller = (MyController) context.getBean("myController");
 
         System.out.println("---------Primary Bean");
