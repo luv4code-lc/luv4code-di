37bbbd9c680f88e642ae304d4d58684a374310f9
luv4boot <luv4boot@gmail.com>
2021-11-18 10:07:00 +0530

    avoid bean name voilation to use @Qualifier annotation

--- src/main/java/com/luv4code/springframework/controllers/SetterInjectedController.java
+++ src/main/java/com/luv4code/springframework/controllers/SetterInjectedController.java
@@ -2,12 +2,14 @@ package com.luv4code.springframework.controllers;
 
 import com.luv4code.springframework.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SetterInjectedController {
     private GreetingService greetingService;
 
+    @Qualifier("setterInjectedGreetingService")
     @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
---
+++ src/main/java/com/luv4code/springframework/services/ConstructorGreetingService.java
@@ -0,0 +1,11 @@
+package com.luv4code.springframework.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConstructorGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello World - Constructor";
+    }
+}
