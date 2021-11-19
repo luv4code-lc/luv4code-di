f230ea210d95ef327be372c9f83f92c72168c78f
luv4boot <luv4boot@gmail.com>
2021-11-19 17:07:50 +0530

    to use application.properties file to read required configurations instead of multiple configuration files

--- src/main/java/com/luv4code/springframework/config/PropertyConfig.java
+++ src/main/java/com/luv4code/springframework/config/PropertyConfig.java
@@ -12,15 +12,8 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
-//@PropertySource({"classpath:datasource.properties","classpath:jms.properties"})
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
 
-    @Autowired
-    Environment env;
 
     @Value("${lfc.username}")
     String user;
@@ -43,7 +36,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(env.getProperty("myCustomProperty"));
+        fakeDataSource.setUser(user);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
@@ -58,9 +51,5 @@ public class PropertyConfig {
         return fakeJmsBroker;
     }
 
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;
-    }
+
 }
