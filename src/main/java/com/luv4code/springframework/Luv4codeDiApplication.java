e1a615c332d6334d355eb223258b6ecc189f9366
luv4boot <luv4boot@gmail.com>
2021-11-19 16:35:05 +0530

    adding dummy external properties to the application

--- src/main/java/com/luv4code/springframework/Luv4codeDiApplication.java
+++ src/main/java/com/luv4code/springframework/Luv4codeDiApplication.java
@@ -1,6 +1,7 @@
 package com.luv4code.springframework;
 
-import com.luv4code.springframework.controllers.*;
+import com.luv4code.springframework.controllers.MyController;
+import com.luv4code.springframework.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,27 +11,10 @@ public class Luv4codeDiApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Luv4codeDiApplication.class, args);
-
-        I18NController i18NController = (I18NController) context.getBean("i18NController");
-        System.out.println(i18NController.sayHello());
-
-
         MyController controller = (MyController) context.getBean("myController");
 
-        System.out.println("---------Primary Bean");
-        System.out.println(controller.sayHello());
-
-        System.out.println("---------Property");
-        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) context.getBean("propertyInjectedController");
-        System.out.println(propertyInjectedController.getGreeting());
-
-        System.out.println("---------Setter");
-        SetterInjectedController setterInjectedController = (SetterInjectedController) context.getBean("setterInjectedController");
-        System.out.println(setterInjectedController.getGreeting());
-
-        System.out.println("---------Constructor");
-        ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) context.getBean("constructorInjectedController");
-        System.out.println(constructorInjectedController.getGreeting());
+        FakeDataSource fakeDataSource = context.getBean("fakeDataSource", FakeDataSource.class);
+        System.out.println(fakeDataSource.getUser() + " " + fakeDataSource.getUrl());
     }
 
 }
